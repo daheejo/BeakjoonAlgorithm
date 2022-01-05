24c0a75eb28857e4a438e34c397fa6f71ffd6887
Dahee Jo <happyj6528@gmail.com>
2022-01-05 11:02:29 +0900

    trim the input

--- 2562.py
+++ 2562.py
@@ -9,6 +9,12 @@ f=int(sys.stdin.readline())
 g=int(sys.stdin.readline())
 h=int(sys.stdin.readline())
 i=int(sys.stdin.readline())
+
+# 입력이 많을 때 코드 간결히 정리!!
+#num_list = []
+#for i in range(9):
+    #num_list.append(int(input()))
+    
 li=[a, b, c, d, e, f, g, h, i]
 ma=max(li)
 print(ma, li.index(ma), sep="\n")  #인덱스 메서드!!
