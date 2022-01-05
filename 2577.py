5a6e303b0ee57cc443e39ca2b800601a91db6ae5
Dahee Jo <happyj6528@gmail.com>
2022-01-05 10:56:30 +0900

    another solution for 2577
    짧고 간결한 코드를 지향하자

--- 2577.py
+++ 2577.py
@@ -6,3 +6,11 @@ val=str(a*b*c)
 print(val.count('0'),val.count('1'),val.count('2'),val.count('3'),
       val.count('4'),val.count('5'),val.count('6'),val.count('7'),
       val.count('8'),val.count('9'), sep="\n")  #.count() 메서드!
+#WEAKNESS#
+#: print문에 val.count()의 반복.. this is NOT GOOD
+
+## another solution ----------------------------------------------------
+#val을 iterable한 객체로(여기선 list) 만들고 반복문 이용
+# val=list(str(a*b*c)
+# for i in range(10)  # 0-9
+#     print(val.count(str(i)))
