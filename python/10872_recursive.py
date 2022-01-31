e5a207c99790d81fdacf1829ceae619b0aa24c4d
Dahee Jo <happyj6528@gmail.com>
2022-01-31 16:48:20 +0900

    seperated folder

---
+++ python/10872_recursive.py
@@ -0,0 +1,21 @@
+# 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+#
+# 출력
+# 첫째 줄에 N!을 출력한다.
+#
+# 예제 입력 1
+# 10
+# 예제 출력 1
+# 3628800
+
+
+def factorial(n):
+    if n <= 0:
+        return 1
+    return factorial(n-1) * n
+
+N = int(input())
+print(factorial(N))
