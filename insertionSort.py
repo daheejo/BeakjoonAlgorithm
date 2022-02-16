1ada31b1f8be7ac83f4f602c4d2aa2665b8962cc
Dahee Jo <happyj6528@gmail.com>
2022-02-16 21:24:50 +0900

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -2,4 +2,12 @@ arr=[1,4,6,5,8,2]
 
 def insertion(array):
     n = len(array)
-    
+    for i in range(1,n):
+        for j in range(1,0,-1):
+            if array [j-1]>array[j]:
+                array[j-1],array[j]=array[j],array[j-1]
+        print(array[:i+1])
+
+print("before: ", arr)
+insertion(arr)
+print("after: ",arr)
