7269efcc0dac472f38522959cbbb2b0cd3d8c5f1
Dahee Jo <happyj6528@gmail.com>
2022-02-18 22:22:27 +0900

    Update mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -1,4 +1,15 @@
 arr = [4.7,9,21,0,8,6]
 
 def merge(array):
-    
+    if len(array)<2:
+        return array
+    mid = len(array)//2
+    lowArr= merge(array[:mid])
+    highArr= merge(array[mid:])
+
+    mergedArr=[]
+    l = h = 0
+    while l < len(lowArr) and h<len(highArr):
+        if lowArr[l]<highArr[h]:
+            mergedArr.append(lowArr[l])
+            l +=1
